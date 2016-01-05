5c1b4f48c1a1570c2c897578768656ef72d1006e
Max Lapides <mlapides@zephyrhealth.com>
2016-01-04 17:51:41 -0800

    Starting work on making this a real Node module

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ var templateInfo = utils.requireAndInit('templateInfo');
 // add config to cache
 cache.put('config', config);
 
-module.exports = function(event) {
-
-    return templateInfo.getTplNames(event.path)
+module.exports = function(filePath) {
+    var tplRootPath = (filePath && _.isString(filePath)) ? filePath : (__dirname + '/templates');
+    return templateInfo.getTplPaths(tplRootPath)
         .then(buildEmails)
         .catch(function(err) {
             utils.logError(1, err);
@@ -25,14 +25,13 @@ module.exports = function(event) {
 
 function buildEmails(templates) {
 
-    var promises = [];
+    var buildPromises = _.reduce(templates, function(builds, tpl) {
+        var build = new Build(tpl);
+        builds.push(build.go());
+        return builds;
+    }, []);
 
-    _.each(templates, function(tplName) {
-        var build = new Build(tplName);
-        promises.push(build.go());
-    });
-
-    return q.all(promises)
+    return Promise.all(buildPromises)
         .then(function() {
             utils.logSuccess('Emails compiled and saved.');
         })
