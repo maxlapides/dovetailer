af5c4b3aa40b1757297514f23d05ecf771611f33
Max Lapides <mlapides@zephyrhealth.com>
2016-12-21 17:02:21 -0500

    refactor: reorganize code in Build service

--- test/build.test.js
+++ test/build.test.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import cache from 'memory-cache'
+import cheerio from 'cheerio'
 
 import BuildLib from '../lib/build'
 
@@ -12,11 +13,11 @@ const newHtml = (body = '', head = '') => (
 
 test('injectHeadStyles', t => {
     const Build = newBuild()
-    const html = newHtml()
+    const $ = cheerio.load(newHtml())
     const styles = '.selector { color: blue; }'
-    const result = Build.injectHeadStyles(html, styles)
+    const result = Build.injectHeadStyles($, styles)
     const expected = newHtml('', '<style type="text/css">.selector { color: blue; }</style>')
-    t.deepEqual(result, expected)
+    t.deepEqual(result.html(), expected)
 })
 
 test('setDoctype', t => {
@@ -29,35 +30,49 @@ test('setDoctype', t => {
 
 test('injectInlineStyles', t => {
     const Build = newBuild()
-    const html = newHtml('<img src="a.jpg" class="box">')
+    const $ = cheerio.load('<img src="a.jpg" class="box">')
     const styles = '.box { color: blue; height: 20px; width: 30px; }'
-    const result = Build.injectInlineStyles(html, styles)
-    const expected = newHtml('<img src="a.jpg" class="box" style="color: blue; height: 20px; width: 30px;" width="30" height="20">')
+    const result = Build.injectInlineStyles($, styles)
+    const expected = '<img src="a.jpg" class="box" style="color: blue; height: 20px; width: 30px;" width="30" height="20">'
+    t.deepEqual(result.html(), expected)
+})
+
+test('cleanHTMLSpecialChars', t => {
+    const Build = newBuild()
+    const html = newHtml('“”')
+    const result = Build.cleanHTMLSpecialChars(html)
+    const expected = newHtml('&#8220;&#8221;')
     t.deepEqual(result, expected)
 })
 
-test('cleanSpecialChars', t => {
+test('cleanSpecialTextChars', t => {
     const Build = newBuild()
-    Build.html = '“”'
-    Build.htmlDev = ''
-    Build.text = ''
-    Build.cleanSpecialChars()
-    const expected = '&#8220;&#8221;'
-    t.deepEqual(Build.html, expected)
+    const text = '“”‘’…–—'
+    const result = Build.cleanTextSpecialChars(text)
+    const expected = '""\'\'...--'
+    t.deepEqual(result, expected)
 })
 
 test('defaultAttrs: table', t => {
     const Build = newBuild()
-    const html = newHtml('<table><tr><td>test</td></tr></table>')
-    const result = Build.defaultAttrs(html)
-    const expected = newHtml('<table cellpadding="0" cellspacing="0" border="0"><tr><td>test</td></tr></table>')
-    t.deepEqual(result, expected)
+    const $ = cheerio.load('<table><tr><td>test</td></tr></table>')
+    const result = Build.defaultAttrs($)
+    const expected = '<table cellpadding="0" cellspacing="0" border="0"><tr><td>test</td></tr></table>'
+    t.deepEqual(result.html(), expected)
 })
 
 test('defaultAttrs: img', t => {
     const Build = newBuild()
-    const html = newHtml('<img src="a.jpg">')
-    const result = Build.defaultAttrs(html)
-    const expected = newHtml('<img src="a.jpg" border="0">')
-    t.deepEqual(result, expected)
+    const $ = cheerio.load('<img src="a.jpg">')
+    const result = Build.defaultAttrs($)
+    const expected = '<img src="a.jpg" border="0">'
+    t.deepEqual(result.html(), expected)
+})
+
+test('defaultAttrs: special characters', t => {
+    const Build = newBuild()
+    const $ = cheerio.load(newHtml('©®™'))
+    const result = Build.defaultAttrs($)
+    const expected = newHtml('&#xA9;&#xAE;&#x2122;')
+    t.deepEqual(result.html(), expected)
 })
