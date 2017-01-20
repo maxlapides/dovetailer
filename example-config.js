7ef95f417d543791c0cec5c2d0d88d3f5cf349f5
Max Lapides <mlapides@zephyrhealth.com>
2017-01-20 09:56:41 -0800

    demo config.js file

---
+++ example-config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+function createConfig() {
+
+    const dirs = {
+        templates  : './templates',
+        scss       : './scss',
+        partials   : './partials'
+    }
+
+    return {
+        DIRS: {
+            PARTIALS  : path.join(__dirname, dirs.partials),
+            TEMPLATES : path.join(__dirname, dirs.templates)
+        },
+        FILES: {
+            PARTIALS  : path.join(dirs.partials, '**/*'),
+            SCSS      : path.join(dirs.scss, '**/*'),
+            TEMPLATES : path.join(dirs.templates, '**/*')
+        }
+    }
+
+}
+
+module.exports = createConfig()
