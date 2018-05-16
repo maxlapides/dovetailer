9509ffa6ab7dfd7c3722abb6188f3adb28b397d7
Max Lapides <max.lapides@tonal.com>
2018-05-15 18:09:04 -0700

    update gulp example

--- example-config.js
+++ example-config.js
@@ -3,12 +3,18 @@ const path = require('path')
 function createConfig() {
   const dirs = {
     templates: './templates',
-    scss: './scss'
+    scss: './scss',
+    build: './build',
+    cache: './cache',
+    components: './components'
   }
 
   return {
     DIRS: {
-      TEMPLATES: path.join(__dirname, dirs.templates)
+      TEMPLATES: path.join(__dirname, dirs.templates),
+      COMPONENTS: path.join(__dirname, dirs.components),
+      BUILD: path.join(__dirname, dirs.build),
+      CACHE: path.join(__dirname, dirs.cache)
     },
     FILES: {
       SCSS: path.join(dirs.scss, '**/*'),
--- example-gulpfile.js
+++ example-gulpfile.js
@@ -1,38 +1,41 @@
 const path = require('path')
 const gulp = require('gulp')
+const del = require('del')
 const browserSync = require('browser-sync').create()
 const compiler = require('dovetailer')
-const config = require('./example-config') // your config
+const config = require('./config')
+
+// BUILD METHODS
+
+const compile = async event => {
+  const templatePath =
+    event && event.path ? path.parse(event.path).dir : config.DIRS.TEMPLATES
+  await compiler(templatePath, config.DIRS.COMPONENTS)
+  await reload()
+}
+
+const reload = () => {
+  return browserSync.active ? browserSync.reload() : startServer()
+}
+
+const startServer = () => {
+  return browserSync.init({
+    server: {
+      baseDir: config.DIRS.BUILD,
+      directory: true
+    }
+  })
+}
 
 // GULP TASKS
 
 gulp.task('default', ['compile', 'watch'])
 
-gulp.task('compile', compile)
+gulp.task('clean', () => del([config.DIRS.BUILD, config.DIRS.CACHE]))
+
+gulp.task('compile', ['clean'], compile)
 
 gulp.task('watch', function() {
   gulp.watch(config.FILES.TEMPLATES, compile)
   gulp.watch(config.FILES.SCSS, () => compile)
 })
-
-// BUILD METHODS
-
-function compile(event) {
-  const templatePath =
-    event && event.path ? path.parse(event.path).dir : config.DIRS.TEMPLATES
-  return compiler(templatePath).then(reload)
-}
-
-function reload() {
-  return browserSync.active ? browserSync.reload() : startServer()
-}
-
-function startServer() {
-  const serverConfig = {
-    server: {
-      baseDir: 'build',
-      directory: true
-    }
-  }
-  return browserSync.init(serverConfig)
-}
