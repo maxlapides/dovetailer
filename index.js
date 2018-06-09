5b9941c5d5745b6dc219d6cca9277f7cb86a4b08
Max Lapides <max.lapides@tonal.com>
2018-06-08 17:51:39 -0700

    starting file saver service

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config = require('./lib/config')
 const logger = require('./lib/logger')
 const utils = require('./lib/utils')
 const Build = require('./lib/build')
+const FileSaver = require('./lib/file-saver')
 
 const templateInfo = utils.requireAndInit('templateInfo')
 
@@ -26,6 +27,13 @@ function main(templatesPath, options = {}) {
     })
 }
 
+// TODO
+function compileEmail(tpl) {
+  const build = new Build(tpl)
+  return build.go()
+}
+main.compileEmail = compileEmail
+
 function buildEmails(templates) {
   const buildPromises = _.reduce(
     templates,
@@ -39,6 +47,7 @@ function buildEmails(templates) {
 
   return Promise.all(buildPromises)
     .then(() => {
+      console.log(JSON.stringify(FileSaver.files, 0, 2))
       logger.info('Emails compiled and saved.')
     })
     .catch(err => {
